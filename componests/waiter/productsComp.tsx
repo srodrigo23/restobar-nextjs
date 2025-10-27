6025dd4a5365409030937cb4e8c64f94473e4240
rodres101 <rodrigosergio93@gmail.com>
2025-10-27 06:27:35 -0400

    wings component

--- componests/waiter/productsComp.tsx
+++ componests/waiter/productsComp.tsx
@@ -1,8 +1,14 @@
-import { Tabs, Tab, Card, CardBody } from "@heroui/react";
-interface ProductsCompProps {
-  
-}
- 
+'use client';
+
+import { Tabs, Tab, Card, CardBody } from '@heroui/react';
+import { useEffect, useState } from 'react';
+
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { Checkbox, Button } from '@heroui/react';
+
+interface ProductsCompProps {}
+
 const products = [
   {
     name: 'Alitas',
@@ -25,30 +31,196 @@ const products = [
   //   icon: '...',
   // },
 ];
+
+interface ChickenWingsCompProps {}
+
+const ChickenWingsComp: React.FC<ChickenWingsCompProps> = () => {
+
+  const wingsAmounts = [
+    {
+      id: '10-wings',
+      label: '10 piezas',
+    },
+    {
+      id: '15-wings',
+      label: '15 piezas',
+    },
+    {
+      id: '20-wings',
+      label: '20 piezas',
+    },
+  ];
+
+  
+  const searchValue = (wingsAm, key:string)=>{
+    for(let i=0; i<wingsAm.length; i++){
+      if(wingsAm[i].id=== key){
+        return wingsAm[i].label
+      }
+    }
+  }
+
+  const [sizeSelection, setSizeSelection] = useState<string | null>(null);
+  const [textualOrder, setTextualOrder] = useState<string>('');
+
+  const [isSelectedSpicy, setIsSelectedSpicy] = useState(false);
+  const [isSelectedBBQ, setIsSelectedBBQ] = useState(false);
+  const [isSelectedHoneyMustard, setIsSelectedHoneyMustard] = useState(false);
+
+  const handleSelection = (
+    event: React.MouseEvent<HTMLElement>,
+    newSelection: string | null
+  ) => {
+    setSizeSelection(newSelection);
+  };
+
+  useEffect(() => {
+    
+    if (sizeSelection !== null) {
+      const textualAmount = searchValue(wingsAmounts, sizeSelection);
+      const sauces:string[] = []
+
+      setTextualOrder(textualAmount);
+      
+      if(isSelectedSpicy){
+        sauces.push('Picante')
+      }
+
+      if (isSelectedBBQ) {
+        sauces.push('Barbacoa');
+      }
+
+      if (isSelectedHoneyMustard) {
+        sauces.push('Mostaza con miel');
+      }
+
+      // let saucesText = ''
+      // if(Array.isArray(sauces) && sauces.length > 0){
+      //   saucesText = sauces.join()
+      // }
+      
+      const textualOrder =
+        sauces.length === 0
+          ? `${textualAmount} de alitas`
+          : `${textualAmount} de alitas con (${sauces.join(', ')})`;
+      
+      setTextualOrder(textualOrder)
+    }
+
+
+
+    if (sizeSelection === null) {
+      setTextualOrder('')
+      setIsSelectedSpicy(false);
+      setIsSelectedBBQ(false);
+      setIsSelectedHoneyMustard(false);
+    }
+
+  }, [sizeSelection, isSelectedSpicy, isSelectedBBQ, isSelectedHoneyMustard]);
+
+  const buildChickenWingsOrderItem = () => {
+
+  }
+
+  return (
+    <>
+      <ToggleButtonGroup        value={sizeSelection}
+        exclusive
+        onChange={handleSelection}
+        aria-label='text alignment'
+      >
+        {wingsAmounts.map((element, index) => {
+          return (
+            <ToggleButton
+              key={index}
+              value={element.id}
+              aria-label={element.label}
+            >
+              {element.label}
+            </ToggleButton>
+          );
+        })}
+        {/* <ToggleButton value='left' aria-label='left aligned'>
+          10 piezas
+        </ToggleButton>
+        <ToggleButton value='center' aria-label='centered'>
+          12 piezas
+        </ToggleButton>
+        <ToggleButton value='right' aria-label='right aligned'>
+          14 piezas
+        </ToggleButton> */}
+
+        {/* <ToggleButton value='justify' aria-label='justified' disabled>
+          
+        </ToggleButton> */}
+      </ToggleButtonGroup>
+      <div>
+        <div>Salsas</div>
+
+        <div className='flex gap-2'>
+          <Checkbox
+            isDisabled={sizeSelection === null}
+            isSelected={isSelectedSpicy}
+            onValueChange={setIsSelectedSpicy}
+          >
+            Picante
+          </Checkbox>
+          <Checkbox
+            isDisabled={sizeSelection === null}
+            isSelected={isSelectedBBQ}
+            onValueChange={setIsSelectedBBQ}
+          >
+            Barbacoa
+          </Checkbox>
+          <Checkbox
+            isDisabled={sizeSelection === null}
+            isSelected={isSelectedHoneyMustard}
+            onValueChange={setIsSelectedHoneyMustard}
+          >
+            Mostaza con Miel
+          </Checkbox>
+        </div>
+      </div>
+
+      <div>
+        <Button isDisabled={sizeSelection === null} color='secondary'>Agregar</Button>
+        <div>{textualOrder}</div>
+      </div>
+    </>
+  );
+};
+
 const ProductsComp: React.FC<ProductsCompProps> = () => {
   return (
     <div className='flex flex-col h-full'>
       <div className='font-semibold mb-2'>Productos disponibles</div>
 
-      <Tabs aria-label='Options' className='flex-1 flex flex-col'>
+      <Tabs aria-label='Options' className='flex flex-col'>
         <Tab key='alitas' title='Alitas'>
-          <Card className='h-full'>
-            <CardBody className='h-full overflow-auto'>Alitas</CardBody>
+          <Card>
+            <CardBody>
+              <ChickenWingsComp />
+            </CardBody>
           </Card>
         </Tab>
         <Tab key='cerveza' title='Cerveza Artesanal'>
-          <Card className='h-full'>
-            <CardBody className='h-full overflow-auto'>Cerveza artesanal</CardBody>
+          <Card>
+            <CardBody>Cerveza artesanal</CardBody>
           </Card>
         </Tab>
         <Tab key='soda' title='Gaseosas'>
-          <Card className='h-full'>
-            <CardBody className='h-full overflow-auto'>Gaseosas</CardBody>
+          <Card>
+            <CardBody>Gaseosas</CardBody>
           </Card>
         </Tab>
         <Tab key='juice' title='Jugos'>
-          <Card className='h-full'>
-            <CardBody className='h-full overflow-auto'>Jugos y refrescos</CardBody>
+          <Card>
+            <CardBody>Jugos y refrescos</CardBody>
+          </Card>
+        </Tab>
+        <Tab key='drinks' title='Bebidas'>
+          <Card>
+            <CardBody>Bebidas</CardBody>
           </Card>
         </Tab>
       </Tabs>
@@ -60,6 +232,6 @@ const ProductsComp: React.FC<ProductsCompProps> = () => {
       </ul> */}
     </div>
   );
-}
- 
+};
+
 export default ProductsComp;
