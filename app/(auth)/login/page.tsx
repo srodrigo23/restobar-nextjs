1addab2edae7cb41b8bfa3415677e9ebb841731c
rodres101 <sistemas.shema.OP1@gmail.com>
2025-11-16 17:17:07 -0400

    improve login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,84 +1,103 @@
-'use client'
+'use client';
 
-import { Button } from "@heroui/react"
-import { useForm, SubmitHandler } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import z from "zod"
-// import { error } from "console"
+import { Button } from '@heroui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Input } from '@heroui/react';
+
+import z from 'zod';
 
 // type Inputs = {
 //   username : string
 //   password : string
 // }
 
-const loginFormShema  = z.object({
-    username: z.
-        string().min(5, {
-            message:'El nombre de usuario debe ser al menos de 5 caracteres'
-        })
-        .nonempty({
-            message:'El nombre de usuario no puede estar vacio!'
-        }),
-    password:z.
-        string().min(5, {
-            message:'La contrasenia debe ser al menos de 5 caracteres'
-        }).nonempty({
-            message:'La contraseña no puede estar vacia!'
-        }),
-})
+const loginFormShema = z.object({
+  username: z
+    .string()
+    .min(5, {
+      message: 'El nombre de usuario debe ser al menos de 5 caracteres',
+    })
+    .nonempty({
+      message: 'El nombre de usuario no puede estar vacio!',
+    }),
+  password: z
+    .string()
+    .min(5, {
+      message: 'La contrasenia debe ser al menos de 5 caracteres',
+    })
+    .nonempty({
+      message: 'La contraseña no puede estar vacia!',
+    }),
+});
 
-type LoginFormType = z.infer<typeof loginFormShema>
+type LoginFormType = z.infer<typeof loginFormShema>;
 
-export default function Page (){
+export default function Page() {
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm<LoginFormType>({
+    resolver: zodResolver(loginFormShema),
+    defaultValues: {
+      username: '', //'admin',
+      password: '', //'admin'
+    },
+  });
 
-    const {
-        register,
-        handleSubmit,
-        // watch,
-        formState: { errors },
-    } = useForm<LoginFormType>(
-        {
-            resolver:zodResolver(loginFormShema),
-            defaultValues:{
-                username:'admin',
-                password:'admin'
-            }
-        }
-    )
+  const onSubmit: SubmitHandler<LoginFormType> = (data) => console.log(data);
 
-    const onSubmit: SubmitHandler<LoginFormType> = (data) => console.log(data)
+  const inputAlert = 'text-xs text-red-400';
 
-    // console.log(watch("username")) // watch input value by passing the name of it
-    // console.log(watch("password"))
-
-    const labelStyle = 'text-sm'
-    const inputStyle = 'text-xl border rounded-md p-2'
-    const inputAlert = 'text-xs text-red-400'
-
-
-    return(
-        <div className="font-sans w-full h-screen flex items-center justify-center">
-
-            <form className="flex flex-col w-2/3 lg:w-1/4 md:w-2/5 shadow-xl mx-10  px-5 py-5 rounded-2xl " onSubmit={handleSubmit(onSubmit)}>
-                
-                <h1 className="text-center font-bold text-xl lg:text-2xl">RESTOBAR <br/>LA HERMANDAD</h1>
-                <br/>
-                
-                <label className={labelStyle} htmlFor="username">Usuario</label>
-                <input className={inputStyle} defaultValue="" {...register("username", { required: true })} />
-
-                {errors.username && <span className={inputAlert}>{errors.username.message}</span>}
-                
-                <br/>
-
-                <label className={labelStyle} htmlFor="password">Password</label>
-                <input className={inputStyle} type="password" {...register("password", { required: true })} />
-                
-                {errors.password && <span className={inputAlert}>{errors.password.message}</span>}
-                <br/>
-                <Button className='cursor-pointer' color="primary" type="submit">INICIAR SESION </Button>
-                {/* <input type="submit" /> */}
-            </form>
+  return (
+    <div className='grid grid-cols-1 place-items-center w-full font-sans h-screen justify-center items-center bg-black'>
+      <form
+        className='grid grid-cols-1 w-2/3 md:w-2/5 lg:w-1/5 gap-3'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className='mb-5'>
+          <h1 className='text-center font-bold text-xl lg:text-2xl text-amber-400'>
+            RESTOBAR
+          </h1>
+          <h1 className='text-center font-bold text-3xl lg:text-2xl text-white'>
+            LA HERMANDAD
+          </h1>
         </div>
-    )
+
+        {/* <label className={labelStyle} htmlFor="username">Usuario</label> */}
+        <Input
+          label='Usuario'
+          defaultValue=''
+          {...register('username', { required: true })}
+        />
+
+        {errors.username && (
+          <span className={inputAlert}>{errors.username.message}</span>
+        )}
+
+        {/* <label className={labelStyle} htmlFor="password">Password</label> */}
+        <Input
+          label='Contraseña'
+          type='password'
+          {...register('password', { required: true })}
+        />
+
+        {errors.password && (
+          <span className={inputAlert}>{errors.password.message}</span>
+        )}
+        
+        <Button 
+					className='cursor-pointer mt-5' 
+					color='primary' 
+					type='submit'
+				>
+          INICIAR SESION
+        </Button>
+        
+      </form>
+    </div>
+  );
 }
